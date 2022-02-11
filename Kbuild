7e86bfb453fc4ddd59f61c6fc559365e7b8515b3
Syudagye <syudagye@gmail.com>
2022-02-11 12:23:40 +0100

    Use hardcoded c_flags taken from kernel headers makefiles

--- Kbuild
+++ Kbuild
@@ -1,8 +1,6 @@
 obj-m += $(MODULE_NAME).o
 $(MODULE_NAME)-objs += module-info.o $(MODULE_NAME).rust.o
 
-export c_flags
-
 .PHONY: build-rust
 
 $(src)/target/$(RUST_TARGET)/release/lib$(MODULE_NAME).a: build-rust
--- build.rs
+++ build.rs
@@ -1,35 +1,36 @@
-use std::{env, path::PathBuf};
+use std::{env, path::PathBuf, process::Command};
 
 use bindgen;
 
 const ALLOWED_FUNCTIONS: &[&str] = &["_printk", "__kmalloc", "kfree"];
 
 fn main() {
-    let kernel_build_dir = env::var("KDIR").expect("KDIR env variable not found");
-    let cflags = env::var("c_flags").expect("You need to build for a Kbuild");
-    let cflags = cflags.split(" ").collect::<Vec<&str>>();
-
-    // formatting all -I and -include flags with absolute paths
-    let mut processed_flags = cflags
-        .clone()
-        .into_iter()
-        .filter(|e| e.starts_with("-I"))
-        .map(|e| e.replace("./", &format!("{}/", kernel_build_dir)))
-        .collect::<Vec<String>>();
-    for i in 0..cflags.len() {
-        if cflags[i] == "-include" {
-            // Wierd thing, the include flag should be separated from the file path with a space, but it causes an error here
-            processed_flags.push(cflags[i+1].replace("./", &format!("-include{}/", kernel_build_dir)));
-        }
-    }
-    // Mandatory to build kernel headers
-    processed_flags.push("-D__KERNEL__".to_string());
+    let kernel_build_dir = format!(
+        "/lib/modules/{}/build",
+        String::from_utf8(Command::new("uname").arg("-r").output().unwrap().stdout)
+            .unwrap()
+            .replace("\n", "")
+    );
+    // Flags taken from the kernel headers makefiles
+    let cflags = [
+        &format!("-I{}/arch/x86/include", kernel_build_dir),
+        &format!("-I{}/arch/x86/include/generated", kernel_build_dir),
+        &format!("-I{}/include", kernel_build_dir),
+        &format!("-I{}/arch/x86/include/uapi", kernel_build_dir),
+        &format!("-I{}/arch/x86/include/generated/uapi", kernel_build_dir),
+        &format!("-I{}/include/uapi", kernel_build_dir),
+        &format!("-I{}/include/generated/uapi", kernel_build_dir),
+        &format!("-include{}/include/linux/compiler-version.h", kernel_build_dir),
+        &format!("-include{}/include/linux/kconfig.h", kernel_build_dir),
+        &format!("-include{}/include/linux/compiler_types.h", kernel_build_dir),
+        "-D__KERNEL__"
+    ];
 
     let mut builder = bindgen::Builder::default()
         .header("wrapper.h")
         .use_core()
         .ctypes_prefix("cty")
-        .clang_args(processed_flags.into_iter());
+        .clang_args(cflags.iter());
 
     for i in ALLOWED_FUNCTIONS {
         builder = builder.allowlist_function(i);
